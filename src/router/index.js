33f3805c32aa3b6995d1c2284b2e0bcad6496f22
Baptiste0715 <baptiste.rognon@edu.univ-fcomte.fr>
2022-09-16 14:21:38 +0200

    index.js fini

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import SetupView from '../views/SetupView.vue'
+import index from '../pages/index.vue'
+import simple from '../pages/simple.vue'
+import offres from '../pages/offres.vue'
+import liste from '../pages/liste.vue'
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/',  name: 'HomeView', component: HomeView },
-    { path: '/setup',  name: 'SetupView', component: SetupView },
-    
+    { path: '/index',  name: 'index', component: index },
+    { path: '/simple',  name: 'simple', component: simple },
+    { path: '/offres',  name: 'offres', component: offres },
+    { path: '/liste',  name: 'liste', component: liste },
+   
   ]
 })
 
