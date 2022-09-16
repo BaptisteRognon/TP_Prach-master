48a73bd33ace0a8e0b6eeb0a2d5f22d0c7db8f44
Baptiste0715 <baptiste.rognon@edu.univ-fcomte.fr>
2022-09-16 14:12:04 +0200

    page index

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import SetupView from '../views/SetupView.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/',  name: 'HomeView', component: HomeView },
+    { path: '/setup',  name: 'SetupView', component: SetupView },
+    
+  ]
+})
+
+export default router
