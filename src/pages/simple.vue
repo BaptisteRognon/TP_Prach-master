123bb1017579f59660e2c74575aa708a65d6daa1
Baptiste0715 <baptiste.rognon@edu.univ-fcomte.fr>
2022-09-16 13:46:35 +0200

    init - mise en place des pages

---
+++ src/pages/simple.vue
@@ -0,0 +1,32 @@
+<template >
+     <div class="p-2">
+        <h2>Page simple</h2>
+        <Card v-for="unObjet of desObjets" v-bind="unObjet" :key="unObjet.nom"/>
+
+        <!--- <card v-bind="desObjets[0]"/>
+              card v-bind="desObjets[1]"/>-->
+     </div>
+  </template>
+
+<script setup lang="ts">
+    
+    import card from "../components/card.vue";
+    
+  const props = defineProps({
+    foo:String
+  });
+
+  const desObjets= [{nom:"une card de maison",
+  prix: 45,
+  favori: false,
+  image: "./une.jpg", 
+  nbrSDB: "number",},
+
+  {nom:"card",
+  prix: "number",
+  favori: "Boolean",
+  image: "./une.jpg",
+  nbrSDB: "number",}
+];
+  
+  </script>
