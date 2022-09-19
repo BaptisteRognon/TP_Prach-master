3e41436b5b919bc68cbfa9508ce63e8e204fc45b
Baptiste0715 <baptiste.rognon@edu.univ-fcomte.fr>
2022-09-19 10:09:29 +0200

    création de la page new

---
+++ src/pages/edit/new.vue
@@ -0,0 +1,11 @@
+
+    <script setup lang="ts">
+        import FormulaireOffreMaison from "../../components/FormulairesOffresMaison.vue";
+    </script>
+    
+    <template>
+        <div>
+            <p>New</p>
+            <FormulaireOffreMaison></FormulaireOffreMaison>
+        </div>  
+    </template>
