c0b7f9fc0b76690e8ad79b6847125e3a711949a0
Baptiste0715 <baptiste.rognon@edu.univ-fcomte.fr>
2022-09-19 08:55:19 +0200

    mise à jour de la page liste

--- src/pages/liste-fetch.vue
+++ src/pages/liste-fetch.vue
@@ -1,8 +1,15 @@
-<script setup>
-    const res = await fetch(...)
-    const posts = await res.json()
-    </script>
-    
-    <template>
-      {{ posts }}
-    </template>
+<script setup lang="ts">
+  import Card from "../components/card.vue";
+  
+  const getMaisons = await fetch("./maisons.json")
+
+  const Maisons = await getMaisons.json()
+</script>
+      
+  
+<template>
+  <div>
+      <p>Liste</p>
+     <Card v-for="m in Maisons" :key="m" v-bind="m"></Card>
+  </div>
+</template>
